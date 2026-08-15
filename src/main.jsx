4d6434ddbdbad3124eaf854cf7546c770deda749
setloth <setheweintraub@me.com>
2026-08-15 16:22:22 -0700

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 import SampleBreakdown from './components/SampleBreakdown.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/SampleBreakdown/">
     <Routes>
       <Route index element={<App/>}/>
       <Route path="dev" element={<SampleBreakdown authoring />} />
